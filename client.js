ce65b8476ae3b445c2826a9d13cdc74aa2d85f29
Joshua Vedane <jvedane@gmail.com>
2020-12-14 16:56:25 -0600

    completed base mode. added timeout

--- client.js
+++ client.js
@@ -9,6 +9,7 @@ let secretNumber = randomNumber(0, people.length - 1);
 
 function readyNow() {
     $('body').append(`<h1>Click On: ${people[secretNumber].name}</h1>`);
+    $(document).on('click', '.image', guessWho)
     displayPhotos()
 }
 
@@ -25,5 +26,23 @@ function displayPhotos() {
         let name = people[i].name;
         $('body').append(`<div class="image" data-index="${i}"> <img src="https://github.com/${userName}.png?size=250" alt="Profile image of ${name}"></div>`);
     }
+
+}
+
+function guessWho() {
+    let index = $(this).data('index')
+    console.log(index)
+    if (index === secretNumber) {
+        $(this).children().addClass('winner')
+        setTimeout(function () {
+            alert("VICTORY!! YOU GUESSED IT!! GET READY TO PLAY AGAIN TO PLAY AGAIN");
+        }, 1000);
+        setTimeout(function () {
+            location.reload();
+        }, 3000);
+    } else {
+        alert('no no no no no no no, try again')
+    }
+
 
 }
